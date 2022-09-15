5f0b6e158dc9cf2ede134746b78ca4aba4d297cd
Andrew Skillett <andrewskillett@gmail.com>
2022-09-14 19:34:49 -0700

    pretty much finished api version only

--- server.js
+++ server.js
@@ -3,18 +3,37 @@ const app = express();
 
 const PORT = 8000;
 
-const savage = {
-    'birthName' : 'bill',
-    'birthLocation' : 'down town',
-    'age': 29
+const rappers = {
+    '21 savage': {
+        'birthName' : 'bill',
+        'birthLocation' : 'down town',
+        'age': 29
+    },
+    'eminem':{
+        'birthName' : 'em',
+        'birthLocation' : 'e town',
+        'age': 44
+    },
+    'unknown':{
+        'birthName' : '??????',
+        'birthLocation' : 'unknown',
+        'age': 0
+    }
 }
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/api', (req, res) => {
-    res.json(savage);
+app.get('/api/:rapName', (req, res) => {
+    const rapperName = req.params.rapName.toLowerCase();
+    if(rappers[rapperName]){
+        res.json(rappers[rapperName]);
+    }
+    else{
+        res.json(rappers['unknown']);
+    }
+    
 });
 
 
