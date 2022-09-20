7150d2985340a42ec33cdd0ab53fcde59652ac29
Andrew Skillett <andrewskillett@gmail.com>
2022-09-19 23:27:23 -0700

    not complete, doesn't work. kind of a mess

---
+++ main.js
@@ -0,0 +1,59 @@
+﻿const deleteText = document.querySelectorAll('.fa-trash');
+const thumbText = document.querySelectorAll('fa-thumbs-up');
+
+Array.from(deleteText).forEach((element) => {
+    element.addEventListener('click', deleteRapper);
+});
+
+Array.from(thumbText).forEach((elem) => {
+    elem.addEventListener('click', addLike);
+});
+
+async function deleteRapper() {
+    const sName = this.parentNode.childNodes[1].innerText; //this is 1 3 5 7  instead of usual 0 1 2 3 index because its grabbing
+    //nodes from the DOM 
+    const bName = this.parentNode.childNodes[3].innerText;
+
+    try {
+        const response = await fetch('deleteRapper',
+            {
+                method: 'delete',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    'stageNameS': sName,
+                    'birthNames': bName
+                })
+            })
+        const data = await response.json();
+        console.log(data);
+        location.reload()
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function addLike(){
+    const sName = this.parentNode.childNodes[1].innerText;
+    const bName = this.parentNode.childNodes[3].innerText;
+    const tLikes = Number(this.parentNode.childNodes[5].innerText);
+    
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'stageNameS': sName,
+                'birthNameS': bName,
+                'likeS': tLikes
+            })
+        })
+        const data = await response.json();
+        console.log(data);
+        location.reload();
+    }
+    catch(err){
+        console.error(err);
+    }
+}
--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const rappers = {
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+    // db.collection('rappers').find().sort({likes:-1}).toArray()
+    // .then(data => {    //data will be the array of objects we got back from the db
+    //     res.rend('index.ejs', {info:data});
+    // })
+    //     .catch(err => console.error(err))
 });
 
 app.get('/api/:rapName', (req, res) => {
@@ -36,11 +41,37 @@ app.get('/api/:rapName', (req, res) => {
     
 });
 
-// app.post('/addRapper', (req, res) => {
+// app.post('/addRapper', (req, res) => {  // the /addRapper route is coming from the forms action attribute in our ejs
 //     db.collection('rappers').insertOne({stageName: req.body.stageName, birthName: req.body.birthName, likes: 0})
 //         .then(res => {
 //             console.log('Rapper Added');
 //             res.redirect('/');
+//         })
+//         .catch(err => console.error(err));
+// })
+
+// app.delete('/deleteRapper',(req,res) => {
+//     db.collection('rappers').deleteOne({stageName: req.body.stageNames}) //find object with the name of whatever, and remove this document/obj
+//         .then(res => {
+//             console.log('Rapper Deleted');
+//             res.json('Rapper deleted');
+//         })
+//         .catch(err => console.error(err));
+// })
+
+// app.put('/addOneLike', (req,res) => {
+//     db.collection('rappers').updateOne({stageName : req.body.stageNameS, birthName: req.body.birthNameS, likes: req.body.likeS}, //this will find an object in the data base that matches all those criteria
+//         {
+//             $set: {
+//                 likes: req.body.likeS + 1
+//             }
+//         },{
+//         sort: {_id: -1}, //this is if our array had multiple same values, it'd grab the first one
+//             upsert: false //this is if it doesn't exist, it creates it. both sort and upsert are not necessary to add
+//         })
+//         .then(res => {
+//             console.log('Added one like');
+//             res.json('Like Added');
 //         })
 //         .catch(err => console.error(err));
 // })
